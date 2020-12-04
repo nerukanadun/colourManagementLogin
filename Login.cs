bea663ca3141eecb3b67bdc29f0bdfee611d4e69
Neruka <52091907+nerukanadun@users.noreply.github.com>
2020-12-04 23:58:23 +0530

    Update Login.cs

--- Login.cs
+++ Login.cs
@@ -6,9 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.Data.SqlClient;
-using System.Runtime.InteropServices;
+
 
 
 namespace colourManagement
